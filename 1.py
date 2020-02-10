ea17495a38130ddc7eee7b4d1bbec3b3d16b5866
Andrii Makarenko <andriimakarenko@outlook.com>
2020-02-10 17:09:26 +0200

    Gets the mount point for a disk with enough free space

--- 1.py
+++ 1.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
-def createAndFill(x, y, z):
-	pass
+from os import *
+import subprocess
 
-x = input("Please specify X")
-y = input("Please specify Y")
-z = input("Please specify Z")
+def createAndFill(x, y, z):
+	p1 = subprocess.run(['df', '-l', '-BM'], capture_output=True, text=True)
+	if p1.returncode != 0:
+		raise(Exception("Couldn't complete 'df -l -BM'"))
+
+	goodArr = [] # now this is just ungodly
+	goodMntPoint = "" # but I lack time to clean array properly in-place
+	linesArr = p1.stdout.split("\n")
+	linesArr = linesArr[1:] # remove column names
+	for line in linesArr:
+		if line[:5] == "/dev/":
+			goodArr.append(line)
+
+	for line in goodArr:
+		stats = line.split()
+		mntPoint = stats[-1]
+		freeSpace = int(stats[-3][:-1])
+		if freeSpace > x:
+			goodMntPoint = mntPoint
+			break
+	if goodMntPoint == "":
+		raise(Exception("Could not find a disk with enough free space"))
+
+	
+
+# x = input("Please specify X: ")
+# y = input("Please specify Y: ")
+# z = input("Please specify Z: ")
 
 try:
-	createAndFill(x, y, z)
-except Error as err:
-	print("Couldn't complete the task: ", err)
+	# createAndFill(x, y, z)
+	createAndFill(5, 5, 5)
+except Exception as e:
+	print("Couldn't complete the task: ", e)
