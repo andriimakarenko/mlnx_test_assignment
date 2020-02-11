f7585d68c37a7c4d7d43213d01f0f473a574ccd1
Andrii Makarenko <andriimakarenko@outlook.com>
2020-02-11 03:26:54 +0200

    Remove extra newlines from exception messages

--- exceptions.py
+++ exceptions.py
@@ -3,8 +3,7 @@ class DangerousArgsCombinationException(Exception):
 		self.message = """
 The combination of arguments you provided is dangerous.
 The amount of free space required should be at least enough to fit the files that will be written.
-Therefore, minimum required free space must be equal to or more than amount of files times each file's size.
-"""
+Therefore, minimum required free space must be equal to or more than amount of files times each file's size."""
 	def __str__(self):
 		return self.message
 
@@ -12,7 +11,16 @@ class InsufficientFreeSpaceException(Exception):
 	def __init__(self):
 		self.message = """
 Could not find a mounted partition with enough free space.
-Try setting a smaller minimum required free space. 
-"""
+Try setting a smaller minimum required free space. """
+	def __str__(self):
+		return self.message
+
+# Turns out, subpocess.run handles such exception on its own, so this exception is unneeded for now
+class CommandExecutionException(Exception):
+	def __init__(self, command):
+		self.message = ('Could not execute the following command:\n'
+		f'\'{command}\'\n'
+		'You are probably running this script on a non POSIX-compliant OS (like, maybe, MS Windows)\n'
+		'That is not da wae')
 	def __str__(self):
 		return self.message
